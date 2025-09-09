b898fb86eb2ff94998cebd554451295d03464007
Fabio Vilalba <fabio.vilalba@topcode.com.br>
2025-09-09 16:24:21 -0300

    Iniciado a webapi no projeto

---
+++ Customer.API/Program.cs
@@ -0,0 +1,50 @@
+using Customer.API.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new()
+    {
+        Title = "My API",
+        Version = "v1",
+        Description = "A simple example ASP.NET Core Web API"
+    });
+    
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+});
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+//Registro do repositório
+builder.Services.AddSingleton<CustomerRepository>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
+
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+
+
+app.Run();
