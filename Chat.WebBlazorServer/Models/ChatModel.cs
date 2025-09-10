2df7887c6411e80becd3b3fdf7e35910531a590a
fabiovalinhos <fabiovalinhos@gmail.com>
2025-09-10 17:52:34 -0300

    Criando o projeto do frontend em blazor server

---
+++ Chat.WebBlazorServer/Models/ChatModel.cs
@@ -0,0 +1,19 @@
+namespace Chat.WebBlazorServer.Models
+{
+    public class ChatModel
+    {
+     public ChatModel()
+        {
+
+        }
+        
+        public ChatModel(string systemMessage)
+        {
+            ChatHistory = new ChatHistory(systemMessage);
+        }
+
+        public ChatHistory ChatHistory { get; set; } = [];
+
+        public string Prompt { get; set; } = string.Empty;   
+    }
+}
---
+++ Chat.WebBlazorServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chat.WebBlazorServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
